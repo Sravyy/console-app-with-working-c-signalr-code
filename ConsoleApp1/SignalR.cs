516cd5ac9daad02113f11a104e0db0b711ef06ff
Sravy <svelaga0412@gmail.com>
2018-03-08 16:02:02 -0800

    add testSignalR file with new methods

--- ConsoleApp1/SignalR.cs
+++ ConsoleApp1/SignalR.cs
@@ -20,7 +20,7 @@ namespace ConsoleApp1
         public SignalR()
         {
             var hubConnection = new HubConnection("http://www.contoso.com/");
-            IHubProxy stockTickerHubProxy = hubConnection.CreateHubProxy("StockTickerHub");
+            IHubProxy stockTickerHubProxy = hubConnection.CreateHubProxy("SmsHub");
 
             stockTickerHubProxy.On<Stock>("UpdateStockPrice", stock => Console.WriteLine("Stock update for {0} new price {1}", stock.Symbol, stock.Price));
             ServicePointManager.DefaultConnectionLimit = 10;
---
+++ ConsoleApp1/testSignalR.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class testSignalR
+    {
+        public async void TestSignalR()
+        {
+            var hubConnection = new HubConnection("http://app.crelatedev.com");
+            var smsHubProxy = hubConnection.CreateHubProxy("SmsHub");
+
+            smsHubProxy.On<int, int>("triggerEvent", (userId, numberId) =>
+            {
+                //triggerEvent has been called from server
+            });
+
+            try
+            {
+                await hubConnection.Start();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            //Add a button and to this event, write an Invoke method like below
+
+            smsHubProxy.Invoke<>("TriggerEvent", hubConnection.userId, hubConnection.numberId)
+        }   
+
+
+    }
+}
+
+
+public void Init()
+{
+    IHubProxy _hub;
+
+    var connection = new HubConnection("http://192.168.43.116:8088/");
+    _hub = connection.CreateHubProxy("ChatHub");
+    connection.Start().Wait();
+
+    _hub.On<string, string>("MessageReceived", (x, y) =>
+    {
+        x += y;
+        signalRMess = x;
+        MessagingCenter.Send<InvokeSignalR, string>(this, "SignalR", signalRMess);
+    });
+
+
+}
